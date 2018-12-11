01ab92a0674474633e7949f93bcfaa24c598a146
opsisdev <45335210+opsisdev@users.noreply.github.com>
2018-12-11 05:27:02 -0800

    Update servicebot.sh

--- servicebot.sh
+++ servicebot.sh
@@ -1,5 +1,5 @@
 wget -qO- https://get.docker.com/
-curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)"
+curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose"
 chmod +x /usr/local/bin/docker-compose
 docker-compose -v
 git clone https://github.com/service-bot/servicebot-deploy.git
